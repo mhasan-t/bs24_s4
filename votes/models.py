de333aaddc08d617dc699b268345013d513c0bde
mhasan-t <tahnoonn19@gmail.com>
2024-02-01 10:31:16 +0600

    Wrote serializers for votes app

--- votes/models.py
+++ votes/models.py
@@ -18,6 +18,7 @@ class Winner(models.Model):
     item_id = models.ForeignKey(OfferedItem, on_delete=models.CASCADE)
     won_on = created_at = models.DateTimeField(default=datetime.now)
     casted_votes = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self) -> str:
         return f'Menu [{self.item_id}] won [{self.won_on} with {self.casted_votes} votes.]'
---
+++ votes/serializers.py
@@ -0,0 +1,23 @@
+from rest_framework import serializers
+from django.contrib.auth.hashers import make_password
+
+from accounts.serializers import UserSerializer
+from restaurants.serializers import OfferedItemSerializer
+from .models import Vote, Winner
+
+
+class VoteSerializer(serializers.HyperlinkedModelSerializer):
+    user_id = UserSerializer(many=False, read_only=True)
+    item_id = OfferedItemSerializer(many=False, read_only=True)
+
+    class Meta:
+        model = Vote
+        fields = ['id', 'created_at', 'user_id', 'item_id']
+
+
+class WinnerSerializer(serializers.HyperlinkedModelSerializer):
+    item_id = OfferedItemSerializer(many=False, read_only=True)
+
+    class Meta:
+        model = Vote
+        fields = ['id', 'created_at', 'won_on', 'item_id']
