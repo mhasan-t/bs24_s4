958a34cd7205f947e8193125759dd8ed9d3136eb
mhasan-t <tahnoonn19@gmail.com>
2024-02-01 10:26:53 +0600

    Wrote models for votes app

--- bs23_s4/settings.py
+++ bs23_s4/settings.py
@@ -42,7 +42,8 @@ INSTALLED_APPS = [
 
     # project apps
     'accounts',
-    'restaurants'
+    'restaurants',
+    'votes'
 ]
 
 MIDDLEWARE = [
--- votes/models.py
+++ votes/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from datetime import datetime
 
-# Create your models here.
+from restaurants.models import OfferedItem
+from accounts.models import User
+
+
+class Vote(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    item_id = models.ForeignKey(OfferedItem, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return f'User [{self.user_id}] voted for item [{self.item_id}]'
+
+
+class Winner(models.Model):
+    item_id = models.ForeignKey(OfferedItem, on_delete=models.CASCADE)
+    won_on = created_at = models.DateTimeField(default=datetime.now)
+    casted_votes = models.IntegerField()
+
+    def __str__(self) -> str:
+        return f'Menu [{self.item_id}] won [{self.won_on} with {self.casted_votes} votes.]'
